95d78affb28dcab8896e2698388affa1c2be6c37
KAP <KAP@DESKTOP-DNB9NIU>
2023-08-03 13:50:52 +0300

    Добавьте файлы проекта.

---
+++ LightForm/LightForm.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace LightForm
+{
+    public class LightForm : Form
+    {
+
+        public LightForm() : base()
+        {
+            WindowState = FormWindowState.Normal;
+            FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}
---
+++ LightForm/SvgElement/Svg.cs
@@ -0,0 +1,69 @@
+﻿using LightForm.Annotations;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+
+namespace LightForm.SvgElement
+{
+    public class Svg : INotifyPropertyChanged
+    {
+
+        public string Data 
+        {
+            get => _data;
+            set
+            {
+                if (value == _data) return;
+                _data = value;
+                OnPropertyChanged();
+            }
+        } private string _data;
+
+        public Size Size 
+        {
+            get => _size;
+            set
+            {
+                if (value.Equals(_size)) return;
+                _size = value;
+                OnPropertyChanged();
+            }
+        } private Size _size;
+
+        private Bitmap bmp;
+
+        public Svg()
+        {
+            Size = Size.Empty;
+            Data = "";
+        }
+
+        public Svg(string data)
+        {
+            Data = data;
+            Size = new Size(16, 16);
+        }
+
+        public Bitmap Draw()
+        {
+            bmp = new Bitmap(Size.Width, Size.Height, PixelFormat.Format32bppArgb);
+
+
+
+            return bmp;
+        }
+
+        #region event notify
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
